f09859008fe2d8dc1633aec537604f0eb00fc0e2
jnates <nates1999123@gmail.com>
2023-03-12 19:31:20 -0500

    Mejora código existente

--- infrastructure/router.products.go
+++ infrastructure/router.products.go
@@ -20,6 +20,6 @@ func RoutesProducts(conn *database.DataDB) http.Handler {
 func routesProduct(handler *v1.ProductRouter) http.Handler {
 	router := chi.NewRouter()
 	router.Post("/", handler.CreateProductHandler)
-	router.Get("/", handler.GetProductHandler())
+	router.Get("/", handler.GetProductHandler)
 	return router
 }
